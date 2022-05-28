c71250d49c554cf00ad60ff09a80ed33cc17da86
BrilAlex <brilalex.it@yandex.ru>
2022-05-28 12:51:26 +0300

    Registration state added

--- src/sc2-features/f1-auth/Registration/Registration.tsx
+++ src/sc2-features/f1-auth/Registration/Registration.tsx
@@ -1,27 +1,30 @@
 import {Button} from "../../../sc1-main/m1-ui/common/components/c2-Button/Button";
 import {InputText} from "../../../sc1-main/m1-ui/common/components/c1-InputText/InputText";
+import {useState} from "react";
 
 export const Registration = () => {
+  const [email, setEmail] = useState("mail@mail.com");
+  const [password, setPassword] = useState("123qwrty");
+  const [confirmPassword, setConfirmPassword] = useState("123qwrty");
+
   return (
     <div>
       <p>Study Cards</p>
       <h1>Registration</h1>
       <div>
         <InputText
-          value={"saasasas"}
-          onChangeText={() => {}}
-          onEnter={() => {}}
+          value={email}
+          onChangeText={setEmail}
         />
         <InputText
-          value={"adadadadd"}
-          onChangeText={() => {}}
-          onEnter={() => {}}
+          value={password}
+          onChangeText={setPassword}
           type={"password"}
         />
         <InputText
-          value={""}
-          onChangeText={() => {}}
-          onEnter={() => {}}
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+          type={"password"}
         />
         <Button onClick={() => {}}>Cancel</Button>
         <Button onClick={() => {}}>Register</Button>
