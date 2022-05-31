f3de57e18303174c5a62234abb65c168ebab753c
itbali <alex.itbali@gmail.com>
2022-05-31 10:18:06 +0300

    showing progress bar only if loading added setLoader in loginReducer

--- src/sc2-features/f1-auth/Login/Login.tsx
+++ src/sc2-features/f1-auth/Login/Login.tsx
@@ -2,10 +2,17 @@ import {LoginForm} from "./LoginForm";
 import s from "../../../sc1-main/m1-ui/App.module.css";
 import {ProgressBar} from "../../../sc1-main/m1-ui/common/components/ProgressBar/ProgressBar";
 import {useAppSelector} from "../../../sc1-main/m2-bll/store";
+import {Navigate} from "react-router-dom";
+import {PATH} from "../../../sc1-main/m1-ui/Main/Pages";
 
 export const Login = () => {
 
   const appIsLoading = useAppSelector(state => state.app.appIsLoading)
+  const user_ID = useAppSelector(state => state.profile.user._id);
+
+  if (user_ID) {
+    return <Navigate to={PATH.PROFILE}/>
+  }
 
   return (
     <div className={s.smallContainer}>
