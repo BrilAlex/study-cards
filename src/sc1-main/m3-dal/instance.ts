90a4763f2d13e1dd85c74347b348cc9406df3b28
BrilAlex <brilalex.it@yandex.ru>
2022-06-02 20:35:38 +0300

    Changed baseURL in instance.ts

--- src/sc1-main/m3-dal/instance.ts
+++ src/sc1-main/m3-dal/instance.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-    // baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/', // для запросов на смену пароля менять урл на https://neko-back.herokuapp.com/2.0/
-    baseURL: 'https://neko-back.herokuapp.com/2.0/',
+    baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/', // для запросов на смену пароля менять урл на https://neko-back.herokuapp.com/2.0/
     withCredentials: true,
 })
