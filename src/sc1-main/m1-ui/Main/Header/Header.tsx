8e45a187188305766c20638de87ab2f60043c127
itbali <alex.itbali@gmail.com>
2022-06-04 00:45:41 +0300

    added mobile menu added burger button styled mobile menu seperated mobileMenu and FullscreenMenu if two different files

--- src/sc1-main/m1-ui/Main/Header/Header.tsx
+++ src/sc1-main/m1-ui/Main/Header/Header.tsx
@@ -1,43 +1,29 @@
-import {NavLink} from "react-router-dom";
-import {PATH} from "../Pages";
-import s from "./Header.module.css";
-import {LogoutThunkTC} from "../../../../sc2-features/f1-auth/Login/bll/loginReducer";
-import {useAppDispatch, useAppSelector} from "../../../m2-bll/store";
-import {Button} from "../../common/components/c2-Button/Button";
+import {useEffect, useState} from "react";
+import {HeaderMobile} from "./HeaderMobile";
+import {HeaderBigScreen} from "./HeaderBigScreen";
 
 export const Header = () => {
-  const user_ID = useAppSelector<string>(state => state.profile.user._id);
-  const dispatch = useAppDispatch();
+  const [isMobile, setIsMobile] = useState(false)
 
-  const getNavLinkStyle = (NavData: { isActive: boolean }) => {
-    return NavData.isActive ? `${s.item} ${s.active}` : s.item;
-  };
+  window.addEventListener('resize', () => {
+    if (window.innerWidth < 800) {
+      setIsMobile(true)
+    } else {
+      setIsMobile(false)
+    }
+  })
 
-  const logoutHandler = () => {
-    dispatch(LogoutThunkTC());
-  }
+  useEffect(() => {
+    if (window.innerWidth < 800) {
+      setIsMobile(true)
+    } else setIsMobile(false)
+  }, [])
 
-  return (
-    <header>
-      <div className={s.navbar}>
-        <h1 className={s.logo}>NINJAS</h1>
-        <NavLink to={PATH.LOGIN} className={(NavData) => getNavLinkStyle(NavData)}>
-          Login
-        </NavLink>
-        <NavLink to={PATH.REGISTRATION} className={(NavData) => getNavLinkStyle(NavData)}>
-          Registration
-        </NavLink>
-        <NavLink to={PATH.PROFILE} className={(NavData) => getNavLinkStyle(NavData)}>
-          Profile
-        </NavLink>
-        <NavLink to={PATH.PASSWORD_RECOVERY} className={(NavData) => getNavLinkStyle(NavData)}>
-          Password recovery
-        </NavLink>
-        <NavLink to={PATH.CREATE_NEW_PASSWORD} className={(NavData) => getNavLinkStyle(NavData)}>
-          Create new password
-        </NavLink>
-        {user_ID && <Button onClick={logoutHandler} style={{margin: "15px 15px", padding: "5px 10px"}}>Logout</Button>}
-      </div>
-    </header>
-  );
-};
+  return (<>
+      {isMobile
+        ? <HeaderMobile/>
+        : <HeaderBigScreen/>
+      }
+    </>
+  )
+}
---
+++ src/sc1-main/m1-ui/Main/Header/HeaderMobile.tsx
@@ -0,0 +1,57 @@
+import {NavLink} from "react-router-dom";
+import {PATH} from "../Pages";
+import s from "./Header.module.css";
+import {LogoutThunkTC} from "../../../../sc2-features/f1-auth/Login/bll/loginReducer";
+import {useAppDispatch, useAppSelector} from "../../../m2-bll/store";
+import {Button} from "../../common/components/c2-Button/Button";
+import {useState} from "react";
+
+export const HeaderMobile = () => {
+
+  const [isBurgerMenuOpened, setIsBurgerMenuOpened] = useState(false)
+  const user_ID = useAppSelector<string>(state => state.profile.user._id);
+  const dispatch = useAppDispatch();
+
+  const getNavLinkStyle = (NavData: { isActive: boolean }) => {
+    return NavData.isActive ? `${s.item} ${s.active}` : s.item;
+  };
+
+  const logoutHandler = () => {
+    dispatch(LogoutThunkTC());
+  };
+
+  const onBurgerMenuButtonClickHandler = () => {
+    setIsBurgerMenuOpened(!isBurgerMenuOpened)
+  }
+  return (
+    <header>
+      <div className={s.navbar}>
+        <h1 className={s.logo}>NINJAS</h1>
+        <div className={isBurgerMenuOpened ? s.mobileLinksOpen : s.mobileLinksClosed}>
+          <NavLink to={PATH.LOGIN} className={(NavData) => getNavLinkStyle(NavData)}>
+            Login
+          </NavLink>
+          <NavLink to={PATH.REGISTRATION} className={(NavData) => getNavLinkStyle(NavData)}>
+            Registration
+          </NavLink>
+          <NavLink to={PATH.PROFILE} className={(NavData) => getNavLinkStyle(NavData)}>
+            Profile
+          </NavLink>
+          <NavLink to={PATH.PASSWORD_RECOVERY} className={(NavData) => getNavLinkStyle(NavData)}>
+            Password recovery
+          </NavLink>
+          <NavLink to={PATH.CREATE_NEW_PASSWORD} className={(NavData) => getNavLinkStyle(NavData)}>
+            Create new password
+          </NavLink>
+
+        </div>
+        {user_ID && <Button onClick={logoutHandler} style={{margin: "15px", marginLeft: 'auto', padding: "5px 10px"}}
+		>Logout</Button>}
+        <Button
+          style={{position: 'relative', margin: "15px", padding: "5px 10px", zIndex: 100, backgroundColor: "black"}}
+          onClick={onBurgerMenuButtonClickHandler}>☰</Button>
+      </div>
+
+    </header>
+  )
+}
