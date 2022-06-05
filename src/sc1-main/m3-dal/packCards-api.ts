384bb95e25c52c57a96452937a07d3bbaed43940
ipavka <ipavka@ya.ru>
2022-06-05 15:37:08 +0300

    added UI simple output & types PacksList

--- src/sc1-main/m3-dal/packCards-api.ts
+++ src/sc1-main/m3-dal/packCards-api.ts
@@ -1,8 +1,42 @@
 import {instance} from "./instance";
 
+export type PacksType = {
+  _id: string
+  user_id: string
+  user_name: string
+  private: boolean
+  name: string
+  path: string
+  grade: number
+  shots: number
+  deckCover: string
+  cardsCount: number
+  type: string
+  rating: number // useless o_O
+  created: string
+  updated: string
+  more_id: string
+}
+export type cardPacksDataType = {
+  cardPacks: PacksType[]
+
+  // we need it types ?!
+  cardPacksTotalCount: number
+  maxCardsCount: number
+  minCardsCount: number
+  page: number
+  pageCount: number
+  token: string
+  tokenDeathTime: Date
+ }
+
+
 export const packCardsApi = {
   getAllCards() {
-    return instance.get<any>(`/cards/pack`)
-      .then(res => res.data)
+    return instance.get<cardPacksDataType>(`/cards/pack`)
+      .then(res => {
+        console.log(res)
+        return res.data
+      })
   },
 }
