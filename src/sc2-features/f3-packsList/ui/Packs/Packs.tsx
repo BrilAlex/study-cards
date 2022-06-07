da2218810692d34a3cbdb5174a91b219a9c9533b
Oleg <viktorovich959595@mail.ru>
2022-06-07 15:09:34 +1000

    added 'delete' button  in component pack

--- src/sc2-features/f3-packsList/ui/Packs/Packs.tsx
+++ src/sc2-features/f3-packsList/ui/Packs/Packs.tsx
@@ -4,23 +4,30 @@ import {PacksType} from "../../../../sc1-main/m3-dal/packCards-api";
 import s from './Packs.module.css'
 import {NavLink} from "react-router-dom";
 import {PATH} from "../../../../sc1-main/m1-ui/Main/Pages";
+import {Button} from "../../../../sc1-main/m1-ui/common/components/c2-Button/Button";
 
 type PacksPropsType = {
-  dataPack: PacksType
+    dataPack: PacksType
 }
 
 export const Packs: React.FC<PacksPropsType> = ({dataPack}) => {
-  return (
-    <>
-      <div style={{width: "20%"}}>
-        <NavLink to={PATH.CARDS_LIST + dataPack._id}>
-          {dataPack.name}
-        </NavLink>
-      </div>
-      <div style={{width: "12%"}}>{dataPack.cardsCount}</div>
-      <div style={{width: "20%"}}><BeautyDate date={dataPack.updated}/></div>
-      <div style={{width: "18%"}}>{dataPack.user_name}</div>
-      <div style={{width: "30%"}}>*Action buttons*</div>
-    </>
-  );
+
+    const deletePackCardsHandler = () => {
+        console.log(dataPack._id)
+    }
+    return (
+        <>
+            <div style={{width: "20%"}}>
+                <NavLink to={PATH.CARDS_LIST + dataPack._id}>
+                    {dataPack.name}
+                </NavLink>
+            </div>
+            <div style={{width: "12%"}}>{dataPack.cardsCount}</div>
+            <div style={{width: "20%"}}><BeautyDate date={dataPack.updated}/></div>
+            <div style={{width: "18%"}}>{dataPack.user_name}</div>
+            <div style={{width: "30%"}}>
+                <Button onClick={deletePackCardsHandler} red={true}>Delete</Button>
+            </div>
+        </>
+    );
 };
