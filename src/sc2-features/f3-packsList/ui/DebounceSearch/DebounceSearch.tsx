dfe0694bccfd316058cd6ce51fd9c59ad35c4f5e
ipavka <ipavka@ya.ru>
2022-06-08 17:46:59 +0300

    fix bug DebounceSearch

--- src/sc2-features/f3-packsList/ui/DebounceSearch/DebounceSearch.tsx
+++ src/sc2-features/f3-packsList/ui/DebounceSearch/DebounceSearch.tsx
@@ -1,22 +1,24 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {InputText} from "../../../../sc1-main/m1-ui/common/components/c1-InputText/InputText";
 import s from './DebounceSearch.module.css'
-import {getCardsPackThunk, searchCardsPackThunk} from "../../bll/packsListReducer";
+import {searchCardsPackThunk} from "../../bll/packsListReducer";
 import {useAppDispatch} from "../../../../sc1-main/m2-bll/store";
-import {Button} from "../../../../sc1-main/m1-ui/common/components/c2-Button/Button";
 
 export const DebounceSearch = () => {
 
   const dispatch = useAppDispatch();
   const [searchValue, setSearchValue] = useState('');
   const [info, setInfo] = useState<string>('');
+  const [focusInput, setFocusInput] = useState(false);
 
   useEffect(() => {
     debouncedLogging(searchValue);
   }, [searchValue])
 
   useEffect(() => {
-    if (searchValue) startSearch(searchValue);
+    if (focusInput) {
+      startSearch(searchValue);
+    }
   }, [info])
 
   const startSearch = (value: string) => {
@@ -32,19 +34,14 @@ export const DebounceSearch = () => {
 
   const debouncedLogging = Debounced(setInfo, 1000);
 
-  const cancelHandler = () => {
-    setSearchValue('');
-    dispatch(getCardsPackThunk());
-  }
-
   return (
     <div className={s.mainContent}>
       <InputText placeholder={"Search..."}
                  value={searchValue}
                  onChangeText={setSearchValue}
+                 onFocus={()=> setFocusInput(true)}
+                 onBlur={() => setFocusInput(false)}
       />
-      <Button className={s.buttonStyle}
-              onClick={cancelHandler}>Cancel</Button>
     </div>
   );
 };
