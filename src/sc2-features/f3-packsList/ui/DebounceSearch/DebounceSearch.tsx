3eb6d30bf3a2efaf78c6b8e9f191012e98483be1
ipavka <ipavka@ya.ru>
2022-06-09 18:48:22 +0300

    fix DebounceSearch

--- src/sc2-features/f3-packsList/ui/DebounceSearch/DebounceSearch.tsx
+++ src/sc2-features/f3-packsList/ui/DebounceSearch/DebounceSearch.tsx
@@ -1,46 +1,33 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {InputText} from "../../../../sc1-main/m1-ui/common/components/c1-InputText/InputText";
 import s from './DebounceSearch.module.css'
 import {searchCardsPackThunk} from "../../bll/packsListReducer";
-import {useAppDispatch} from "../../../../sc1-main/m2-bll/store";
+import {AppThunkType, useAppDispatch} from "../../../../sc1-main/m2-bll/store";
 
 export const DebounceSearch = () => {
 
   const dispatch = useAppDispatch();
   const [searchValue, setSearchValue] = useState('');
-  const [info, setInfo] = useState<string>('');
-  const [focusInput, setFocusInput] = useState(false);
 
-  useEffect(() => {
-    debouncedLogging(searchValue);
-  }, [searchValue])
-
-  useEffect(() => {
-    if (focusInput) {
-      startSearch(searchValue);
-    }
-  }, [info])
-
-  const startSearch = (value: string) => {
-    dispatch(searchCardsPackThunk(value));
-  }
   const Debounced = (func: Function, delay: number) => {
     const ref = useRef(0);
-    return (...args: any) => {
+    return (args: AppThunkType) => {
       clearTimeout(ref.current);
-      ref.current = Number(setTimeout(() => func(...args), delay));
+      ref.current = Number(setTimeout(() => func(args), delay));
     };
   };
+  const debounce = Debounced(dispatch, 1000);
 
-  const debouncedLogging = Debounced(setInfo, 1000);
+  const handleInputEvent = (e: React.ChangeEvent<HTMLInputElement>) => {
+    debounce(searchCardsPackThunk(e.currentTarget.value));
+    setSearchValue(e.currentTarget.value)
+  }
 
   return (
     <div className={s.mainContent}>
       <InputText placeholder={"Search..."}
                  value={searchValue}
-                 onChangeText={setSearchValue}
-                 onFocus={()=> setFocusInput(true)}
-                 onBlur={() => setFocusInput(false)}
+                 onChange={handleInputEvent}
       />
     </div>
   );
