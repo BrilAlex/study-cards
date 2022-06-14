1ac04cfb10983fe8fc5cf233b18784688260a742
BrilAlex <brilalex.it@yandex.ru>
2022-06-14 22:29:02 +0300

    Smart random added

--- src/sc2-features/f5-learn/ui/LearnPage.tsx
+++ src/sc2-features/f5-learn/ui/LearnPage.tsx
@@ -15,9 +15,19 @@ export const LearnPage: FC<LearnPagePropsType> = ({cards}) => {
   const cardPackName = useAppSelector<string>(state => state.learn.cardsPackName);
   const navigate = useNavigate();
 
-  const getRandomCard = (array: Array<CardType>) => {
-    const randomIndex = Math.floor(Math.random() * array.length);
-    return array[randomIndex];
+  const getRandomCard = (cards: Array<CardType>) => {
+    const sum = cards.reduce((acc, card) => acc + ((6 - card.grade) ** 3), 0);
+    const randomNumber = Math.random() * sum;
+
+    let s = 0;
+    let i = 0;
+
+    while (s < randomNumber) {
+      s += (6 - cards[i].grade) ** 3;
+      i++;
+    }
+
+    return cards[i - 1];
   };
 
   const randomCard = getRandomCard(cards);
