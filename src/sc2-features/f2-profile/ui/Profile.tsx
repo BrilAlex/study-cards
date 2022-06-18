a3b462de18a6ec38f9804220b76f736fed191e09
ipavka <ipavka@ya.ru>
2022-06-18 18:54:44 +0300

    new Profile

--- src/sc2-features/f2-profile/ui/Profile.tsx
+++ src/sc2-features/f2-profile/ui/Profile.tsx
@@ -1,65 +1,94 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useRef, useState} from 'react';
 import {useAppDispatch, useAppSelector} from "../../../sc1-main/m2-bll/store";
 import {updateNameThunk} from "../bll/profileReducer";
-import {EditModal} from "./EditModal/EditModal";
 import s from './Profile.module.css'
 import {Navigate} from "react-router-dom";
 import {PATH} from "../../../sc1-main/m1-ui/Main/Pages";
 import {UserType} from "../../../sc1-main/m3-dal/profile-api";
 import {ButtonLoad} from "../../../sc1-main/m1-ui/common/components/SpButton/ButtonLoad";
+import {ReactComponent as UploadAva} from '../../../assets/Web_app/add_photo.svg'
+import {ReactComponent as DefaultAva} from '../../../assets/Web_app/robot_ava.svg'
+import {InputText} from "../../../sc1-main/m1-ui/common/components/c1-InputText/InputText";
 
 export const Profile = () => {
 
-  const [name, setName] = useState<string>('');
-  const [activeModal, setActiveModal] = useState(false);
-
   const dispatch = useAppDispatch();
-  const userNameStore = useAppSelector<string>(store => store.profile.user.name);
-  const userData = useAppSelector<UserType>(store => store.profile.user);
-  const userAva = useAppSelector<string | undefined>(store => store.profile.user.avatar);
+  const {name, avatar, email} = useAppSelector<UserType>(store => store.profile.user);
   const isLoading = useAppSelector<boolean>(state => state.profile.loading);
 
+  const [value, setValue] = useState<string>(name);
+  const [error, setError] = useState<string>('');
+  const [newPhoto, setNewPhoto] = useState<any>(undefined); // fix any!?
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const changeName = () => {
-    dispatch(updateNameThunk(name, ''));
-    setActiveModal(false);
-  }
-  const editHandler = () => {
-    setActiveModal(true);
-  }
-  const onFocusHandler = () => {
-    name ? setName(name) : setName(userNameStore)
+    if (newPhoto === avatar) return setError('the same photo');
+    if (name === value && !newPhoto) return setError('nothing has changed');
+    dispatch(updateNameThunk(value, newPhoto));
   }
 
-  if (!userNameStore) {
+  const cancelHandler = () => {
+    setError('')
+    setNewPhoto('');
+  }
+
+  const uploadHandler = () => {
+    inputRef && inputRef.current && inputRef.current.click()
+  }
+
+  const upload = (e: ChangeEvent<HTMLInputElement>) => {
+    if (error) setError('');
+    const newFile = e.target.files && e.target.files[0];
+    if (newFile && newFile.size > 1048576) {
+      return setError('The size of the photo is not more than 1MB');
+    }
+
+    if (newFile) {
+      const reader = new FileReader();
+      reader.onloadend = () => setNewPhoto(reader.result);
+      reader.readAsDataURL(newFile);
+    }
+  }
+
+  if (!name) {
     return <Navigate to={PATH.LOGIN}/>;
   }
 
   return (
     <div className={s.mainBlock}>
-
       <div className={s.editBlock}>
-        <div className={s.edit}>
-          <div className={s.profileAva}>
-            <img src={userAva ? userAva : 'https://clck.ru/WQq57'} alt="user-ava"/>
-          </div>
-          <div className={s.profileInfo}>
-            <h3>{userNameStore}</h3>
-            <h3>{userData.email}</h3>
-            <h3>ID: {userData._id}</h3>
-            <h3>Card Packs: {userData.publicCardPacksCount}</h3>
-            <ButtonLoad isSpinner={isLoading} onClick={editHandler}>Edit</ButtonLoad>
+        <input
+          ref={inputRef}
+          type='file'
+          style={{display: 'none'}}
+          onChange={upload}
+        />
+      </div>
+      <div className={s.edit}>
+        <h1>Personal Information</h1>
+        <div className={s.profileAva}>
+          {!avatar && !newPhoto
+            ? <DefaultAva className={s.defaultAva}/>
+            : <img className={s.defaultAva} src={newPhoto ? newPhoto : avatar} alt="user-ava"/>}
+          <UploadAva className={s.editPhoto} onClick={uploadHandler}/>
+        </div>        <div className={s.profileInfo}>
+          <InputText className={s.profileInput}
+                     value={value}
+                     onChangeText={setValue}
+          />
+          <span className={s.infoSpan}>Nickname</span>
+          <InputText value={email} className={s.profileInput} disabled/>
+          <span className={s.infoSpan}>Email</span>
+          <div className={s.profileError}>
+            {error}
           </div>
         </div>
+        <div className={s.buttonBlock}>
+          <ButtonLoad isSpinner={isLoading} onClick={changeName}>Save</ButtonLoad>
+          <ButtonLoad isSpinner={isLoading} onClick={cancelHandler}>Cancel</ButtonLoad>
+        </div>
       </div>
-
-      <EditModal active={activeModal}
-                 setActive={setActiveModal}
-                 name={userNameStore}
-                 inputValue={name}
-                 setInputValue={setName}
-                 inputFocus={onFocusHandler}
-                 changeName={changeName}
-      />
     </div>
   );
 };
